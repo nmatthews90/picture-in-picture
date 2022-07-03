a7cfbb170fe4241c27d197d943e2167612891acc
Natasha Matthews <natashamatthews90@gmail.com>
2022-07-03 13:32:45 -0500

    added the javascript functions

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## Resources
 [Google Font - Barlow](https://fonts.google.com/specimen/Barlow?query=barl)
+[Picture in picture Article](https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/)
+[Screen Catpure API](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture)
---
+++ script.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//prompt  to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        //catch error here
+        console.log('error here:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+
+//on load
+selectMediaStream();
